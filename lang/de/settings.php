e2e1c5664754c9cc74f7f9dbfcf9bf13eba3c187
Lars Pötter <lists@ing-poetter.de>
2013-08-17 17:20:11 +0200

    translation update

---
+++ lang/de/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Lars Pötter <lists@ing-poetter.de>
+ */
+$lang['rmdir']                 = 'Lösche das temporäre Verzeichnis nachdem das eBook erzeugt wurde';
