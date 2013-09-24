c97ff5c21273b7e7a6d32f0a2d4386c4adfc2449
Chris Jung <kontakt@chrisjung.de>
2013-09-24 09:35:36 +0200

    translation update

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -4,5 +4,8 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Lars Pötter <lists@ing-poetter.de>
+ * @author Chris Jung <kontakt@chrisjung.de>
  */
+$lang['group']                 = 'Name der Gruppe mit Berechtigungen um eBooks zu erzeugen. Diese Einstellung ist optional und wird nur verwendet, wenn ein Benutzer keine Berechtigung zum Hochladen im epub-Namespace hat.';
 $lang['rmdir']                 = 'Lösche das temporäre Verzeichnis nachdem das eBook erzeugt wurde';
+$lang['permalink']             = 'Behalte einen Permalink auf die book creator Seite';
