4e25199af310c7f1dc460c7bee75c60a2db882af
Olivier K <olivier.keshavjee@gmail.com>
2013-12-18 20:00:07 +0100

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Olivier K. <olivier.keshavjee@gmail.com>
+ */
+$lang['button_start']          = 'Start';
+$lang['label_start']           = 'Cliquer sur "Start" pour commencer à créer votre eBook';
+$lang['download']              = 'L\'ebook le plus récent pour cette page est:';
+$lang['download_click']        = 'Pour le télécharger, cliquer sur le lien.';
+$lang['download_alt']          = 'Si vous avez un plugin installé pour lire les ebook, clique-droit sur le lien et sélectionner "Enregistrer sous..."';
+$lang['menu']                  = 'outil de suppression d\'ebook';
+$lang['btn_submit']            = 'Seulement le cache';
+$lang['btn_del']               = 'Cache et ebook(s)';
---
+++ lang/fr/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Olivier K. <olivier.keshavjee@gmail.com>
+ */
+$lang['permalink']             = 'Garder un lien de téléchargement permanant sur la page de création de livre.';
+$lang['less']                  = 'Le compilateur LESS CSS va être utilisé s\'il est présent, à moins que cette option ne soit désactivée.';
