4735e590bbfad4fe54ecec5f76da08bdf3d3e840
Marina Vladi <deldadam@gmail.com>
2014-02-06 20:50:08 +0100

    translation update

---
+++ lang/hu/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Marina Vladi <deldadam@gmail.com>
+ */
+$lang['group']                 = 'Annak felhasználói csoportnak a neve, akik jogosultak e-könyvek készítésére.  A beállítás opcionális, és csak akkor van hatása, ha a felhasználónak nincs feltöltési jogosultsága az epub névtérben.';
+$lang['rmdir']                 = 'Átmeneti könyvtár törlése az e-könyv létrehozása után';
+$lang['permalink']             = 'Állandó letöltési hivatkozás megtartása a könyvet készítő oldalon';
+$lang['less']                  = 'A LESS CSS-fordító használata (ha van), kivéve, ha ez az opció nem kiválasztott';
+$lang['compress']              = 'CSS-stíluslapok tömörítése. Csak akkor van hatása, ha a LESS használata engedett.';
