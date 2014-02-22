193c52f93f937a24adbfdcd37886860814d23745
Myron Turner <turnermm02@shaw.ca>
2014-02-22 10:12:00 -0600

    dutch language updates

---
+++ lang/nl/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Rene <wllywlnt@yahoo.com>
+ */
+$lang['group']                 = 'Naam van de groep die het recht heeft ebooks te vervaardigen. Deze instelling is optioneel en heeft pas effect als een gebruiker geen upload toestemming heeft in de epub directory.';
+$lang['rmdir']                 = 'Verwijder de tijdelijke directory nadat het ebook is vervaardigd.';
+$lang['permalink']             = 'Bewaar de permanente download link op de Book Creator pagina.';
+$lang['less']                  = 'De LESS CSS compiler zal , indien aanwezig, worden gebruikt, tenzij deze instelling van toepassing is.';
+$lang['compress']              = 'Comprimeer CSS style sheet. Alleen beschikbaar indien LESS is toegepast.';
