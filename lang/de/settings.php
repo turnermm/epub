0417974843aeb0d68c0be8f60888d1d5236750aa
Thor Weinreich <thorweinreich@nefkom.net>
2014-03-14 01:10:32 +0100

    translation update

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -5,7 +5,11 @@
  * 
  * @author Lars Pötter <lists@ing-poetter.de>
  * @author Chris Jung <kontakt@chrisjung.de>
+ * @author Thor Weinreich <thorweinreich@nefkom.net>
  */
 $lang['group']                 = 'Name der Gruppe mit Berechtigungen um eBooks zu erzeugen. Diese Einstellung ist optional und wird nur verwendet, wenn ein Benutzer keine Berechtigung zum Hochladen im epub-Namespace hat.';
 $lang['rmdir']                 = 'Lösche das temporäre Verzeichnis nachdem das eBook erzeugt wurde';
 $lang['permalink']             = 'Behalte einen Permalink auf die book creator Seite';
+$lang['less']                  = 'Wenn diese Option aktiviert wird, wird der LESS CSS Compiler verwendet, wenn er vorhanden ist.';
+$lang['compress']              = 'CSS Style-Sheet komprimieren. Diese Funktion ist nur verfügbar, wenn LESS benutzt wird.';
+$lang['sort']                  = 'Normalerweise werden Dateien in einem Namensraum alphabetisch sortiert; ist die Funktion nicht ausgewählt (nicht angekreuzt), werden sie nicht sortiert.';
