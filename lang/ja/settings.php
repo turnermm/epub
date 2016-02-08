b085ec766c6c128a301bc72f83e7b999fa4f137e
Hideaki SAWADA <chuno@live.jp>
2016-02-08 10:40:43 +0100

    translation update

---
+++ lang/ja/settings.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+$lang['group']                 = '電子ブックの作成権限を持つグループ名。この設定はオプションで、ユーザー が epub 名前空間へのアップロード権限を持っていない場合のみ意味があります。';
+$lang['rmdir']                 = '電子ブックを作成後、作業用ディレクトリを削除します。';
+$lang['permalink']             = '本作成者ページ上に恒久的なダウンロードリンクを作成する';
+$lang['less']                  = 'The LESS CSS コンパイラがある場合使用します。';
+$lang['compress']              = 'CSS スタイルシートを圧縮する。LESS を使用している場合のみ利用可能です。';
+$lang['sort']                  = 'デフォルトでは、名前空間内でアルファベット順にソートしてファイルを処理します。チェックしない場合、ディレクトリ順で処理します。';
