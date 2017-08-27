ea5ec988ba77eb6a34694e56acb2d89f7819beb9
2576562185 <2576562185@qq.com>
2017-08-27 11:25:34 +0200

    translation update

---
+++ lang/zh/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author 2576562185 <2576562185@qq.com>
+ */
+$lang['button_start']          = '开始';
+$lang['label_start']           = '单击“开始”来创建你的电子书';
+$lang['download_click']        = '点击链接下载。';
