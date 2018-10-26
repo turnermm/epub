36be99d1f3773733e98540c6dd7e0fdabede6e84
Schplurtz le Déboulonné <schplurtz@laposte.net>
2018-10-26 04:25:07 +0200

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -2,7 +2,8 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
  * @author Olivier K. <olivier.keshavjee@gmail.com>
  * @author Fabrice Dejaigher <fabrice@chtiland.com>
  */
@@ -11,7 +12,7 @@ $lang['label_start']           = 'Cliquer sur "Commencer" pour commencer à cré
 $lang['button_remove']         = 'Supprimer la page de la liste de création';
 $lang['download']              = 'L\'ebook le plus récent pour cette page est:';
 $lang['download_click']        = 'Pour le télécharger, cliquer sur le lien.';
-$lang['download_alt']          = 'Si vous avez un plugin installé pour lire les ebook, clique-droit sur le lien et sélectionner "Enregistrer sous..."';
+$lang['download_alt']          = 'Si vous avez un greffon installé pour lire les ebook, clique-droit sur le lien et sélectionner "Enregistrer sous..."';
 $lang['menu']                  = 'outil de suppression d\'ebook';
 $lang['btn_submit']            = 'Seulement le cache';
 $lang['btn_del']               = 'Cache et ebook(s)';
--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -2,14 +2,19 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
  * @author Olivier K. <olivier.keshavjee@gmail.com>
  * @author Fabrice Dejaigher <fabrice@chtiland.com>
  * @author Nicolas Friedli <nicolas@theologique.ch>
  */
-$lang['group']                 = 'Nom du groupe qui a l\'autorisation de créer des ebooks. Ce paramètre est optionnel et n\'a d\'effet que là où un utilisateur n\'a pas la permission d\'uploader dans l\'espace de nom epub.';
+$lang['group']                 = 'Nom du groupe qui a l\'autorisation de créer des ebooks. Ce paramètre est optionnel et n\'a d\'effet que là où un utilisateur n\'a pas la permission d\'envoyer dans la catégorie epub.';
 $lang['rmdir']                 = 'Supprimer le répertoire de travail lorsque l\'ebook a été créé.';
 $lang['permalink']             = 'Garder un lien de téléchargement permanant sur la page de création de livre.';
 $lang['less']                  = 'Le compilateur LESS CSS va être utilisé s\'il est présent, à moins que cette option ne soit désactivée.';
 $lang['compress']              = 'Compresser la feuille de style CSS. Disponible uniquement si LESS est utilisé.';
 $lang['sort']                  = 'Par défaut, les pages des espaces de nom sont triées alphabétiquement. Si configuré à faux (non coché), elles seront traitées dans l\'ordre du répertoire (non triées).';
+$lang['audio_fn']              = 'Lors de l\'utilisation de fichiers audio, inclure sous les contrôles le nom du fichier audio ou son titre et un lien de note de bas de page vers le fichier lui même';
+$lang['audio_nmsp']            = 'Liste à virgule de catégories où trouver les fichiers audio. Elles peuvent aller jusqu\'à deux niveaux de profondeur. Utilisé seulement pour la création de note de bas de page.';
+$lang['video_nmsp']            = 'Liste à virgule de catégories où trouver les vidéos. Elles peuvent aller jusqu\'à deux niveaux de profondeur. Utilisé seulement pour la création de note de bas de page.';
+$lang['video_fn']              = 'Lors de l\'utilisation de fichiers vidéos, inclure sous les contrôles le nom du fichier audio ou son titre et un lien de note de bas de page vers le fichier lui même';
