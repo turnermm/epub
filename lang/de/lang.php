b7bce96228bad5c064ca57cb4c6a5a1c31ae9166
DokuWiki Translation Tool <admin@dokuwiki.org>
2022-08-05 21:42:39 +0200

    translation update (#35)
    Co-authored-by: Hella <hella.breitkopf@gmail.com>

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -2,15 +2,16 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Hella <hella.breitkopf@gmail.com>
  * @author Lars Pötter <lists@ing-poetter.de>
  * @author Chris Jung <kontakt@chrisjung.de>
  */
 $lang['button_start']          = 'start';
-$lang['label_start']           = 'Auf "start" klicken um ein neues eBook zu erstellen';
-$lang['button_remove']         = 'Seite von der Creator-Liste entfernen';
+$lang['label_start']           = 'Auf "Start" klicken um ein neues eBook zu erstellen';
+$lang['button_remove']         = 'Seite aus der Ersttellerliste entfernen.';
 $lang['download']              = 'Das neueste eBook, welches diese Seite enthält, ist:';
-$lang['download_click']        = 'Zum Downloaden auf den Link klicken';
+$lang['download_click']        = 'Zum Herunterladen auf den Link klicken';
 $lang['download_alt']          = 'Wenn Sie ein eBook-Reader-Plugin installiert haben, klicken Sie rechts auf den Link und wählen sie \'Speichern unter...\'';
 $lang['menu']                  = 'epub-Entfernen-Werkzeug';
 $lang['btn_submit']            = 'nur Cache';
