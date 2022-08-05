43139f4e940b87187a42b1911fbe8ff5b0494f1b
Hella <hella.breitkopf@gmail.com>
2022-08-05 19:00:22 +0200

    translation update

--- lang/de/settings.php
+++ lang/de/settings.php
@@ -2,14 +2,19 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Hella <hella.breitkopf@gmail.com>
  * @author Lars Pötter <lists@ing-poetter.de>
  * @author Chris Jung <kontakt@chrisjung.de>
  * @author Thor Weinreich <thorweinreich@nefkom.net>
  */
 $lang['group']                 = 'Name der Gruppe mit Berechtigungen um eBooks zu erzeugen. Diese Einstellung ist optional und wird nur verwendet, wenn ein Benutzer keine Berechtigung zum Hochladen im epub-Namespace hat.';
-$lang['rmdir']                 = 'Lösche das temporäre Verzeichnis nachdem das eBook erzeugt wurde';
-$lang['permalink']             = 'Behalte einen Permalink auf die book creator Seite';
-$lang['less']                  = 'Wenn diese Option aktiviert wird, wird der LESS CSS Compiler verwendet, wenn er vorhanden ist.';
+$lang['rmdir']                 = 'Löschen Sie das temporäre Verzeichnis nachdem das eBook erzeugt wurde';
+$lang['permalink']             = 'Behalten Sie einen Permalink auf die Buch-Ersteller-Seite';
+$lang['less']                  = 'LESS CSS Compiler verwenden (wenn vorhanden).';
 $lang['compress']              = 'CSS Style-Sheet komprimieren. Diese Funktion ist nur verfügbar, wenn LESS benutzt wird.';
 $lang['sort']                  = 'Normalerweise werden Dateien in einem Namensraum alphabetisch sortiert; ist die Funktion nicht ausgewählt (nicht angekreuzt), werden sie nicht sortiert.';
+$lang['audio_fn']              = 'Wenn Sie Audiodateien verwenden wollen, fügen Sie den Namen der Datei oder den Titel und einen Fußnoten-Link zur Audiodatei selber unterhalb der Audio-Steuerung ein.';
+$lang['audio_nmsp']            = 'Komma-separierte Liste der Namensräume, in denen Audio-Dateien gefunden werden können. Diese können bis zu zwei Ebenen tief sein. Wird nur verwendet, wenn Fußnoten erstellt werden.';
+$lang['video_nmsp']            = 'Komma-separierte Liste der Namensräume, in denen Video-Dateien gefunden werden können. Diese können bis zu zwei Ebenen tief sein. Wird nur verwendet, wenn Fußnoten erstellt werden.';
+$lang['video_fn']              = 'Wenn Sie Videodateien verwenden wollen, fügen Sie den Namen der Datei oder den Titel und einen Fußnoten-Link zur Videodatei selber unterhalb der Video-Steuerung ein.';
