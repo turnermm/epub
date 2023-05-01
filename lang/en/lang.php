bcfd4c9134d6a0c3bd3e2d55b00c5ba04d182c31
гей порно <gloriabardin@freenet.de>
2023-05-01 02:00:13 +0200

    translation update

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,10 +1,20 @@
 <?php
-$lang['button_start'] = 'Start';
-$lang['label_start'] = 'Click "Start" to begin creating your eBook';
-$lang['button_remove'] = 'Remove page from Creator List';
-$lang['download'] ="The most recent ebook for this page is:";
-$lang['download_click'] ="To download it, click on the link. "; 
-$lang['download_alt'] = " If you have an ebook reader plugin installed, right-click on the link and select 'Save . . . As'.";
-$lang['menu'] = 'epub removal tool'; 
-$lang['btn_submit'] = 'Cache Only';
-$lang['btn_del'] = 'Cache and Ebook(s)';
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author гей порно <gloriabardin@freenet.de>
+ */
+$lang['button_start']          = 'It\'s very straightforward to find out any topic on net as compared to books,
+as I found this article at this website.';
+$lang['label_start']           = 'It\'s very straightforward to find out any topic on net as compared to books, 
+as I found this article at this website.';
+$lang['button_remove']         = 'Gloria';
+$lang['download']              = 'It\'s very straightforward to find out any topic on net as compared 
+to books, as I found this article at this website.';
+$lang['download_click']        = 'https://1000otzyvov.com/';
+$lang['download_alt']          = 'https://1000otzyvov.com/';
+$lang['menu']                  = 'It\'s very straightforward to find out any topic on net as compared to books, as I found this article at this website.';
+$lang['btn_submit']            = 'It\'s very straightforward to find out any topic on net 
+as compared to books, as I found this article at this website.';
+$lang['btn_del']               = 'It\'s very straightforward to find out any topic on net as compared to books, as I found this article at this website.';
--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,12 +1,21 @@
 <?php
- $lang['group']='Name of group which has permission to create ebooks.  This setting is optional and has effect only where a user does not have upload permission to the epub namespace.' ;
- $lang['rmdir'] = "Remove scratch directory after the ebook has been created.";
- $lang['permalink'] = "Keep a permanent download link on the book creator page";
- $lang['less'] = "The LESS CSS compiler will be used if present, unless this option is set to false.";
- $lang['compress'] = "Compress CSS style sheet.  Available only when LESS is used.";
- $lang['sort'] = 'By default files processed in namespaces will be sorted alphabetically; if this is set to false (unchecked), they will be processed in directory order (unsorted).';
-$lang['audio_fn'] = 'When  using audio files, include the name of the audio file or its title and a footnote link to the audio file itself beneath the controls';
- $lang['audio_nmsp'] = 'Comma separated list of namespaces where audio files will be found.  These can be up to two levels deep. Used only when creating footnotes.';
-$lang['video_nmsp'] = 'Comma separated list of namespaces where video files will be found.  These can be up to two levels deep. Used only when creating footnotes.';
-$lang['video_fn'] = 'When  using video files, include the name of the  file or its title and a footnote link to the video file itself beneath the controls';
-  
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author гей порно <gloriabardin@freenet.de>
+ */
+$lang['group']                 = 'Gloria';
+$lang['rmdir']                 = 'It\'s very straightforward to find out any topic on net as compared to books, 
+as I found this article at this website.';
+$lang['permalink']             = 'https://1000otzyvov.com/';
+$lang['less']                  = 'It\'s very straightforward to find out any 
+topic on net as compared to books, as I found this article 
+at this website.';
+$lang['compress']              = 'It\'s very straightforward to find out any topic on net as 
+compared to books, as I found this article at this website.';
+$lang['sort']                  = 'Worms Weinsheim';
+$lang['audio_fn']              = 'Gloria';
+$lang['audio_nmsp']            = 'Gloria';
+$lang['video_nmsp']            = 'Gloria';
+$lang['video_fn']              = 'Gloria';
